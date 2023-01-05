8a427d1e6c1b6167adf3fa7e9b7dd8bef1f49926
curegit <37978051+curegit@users.noreply.github.com>
2023-01-05 19:39:55 +0900

    Add download test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+function upload(event: MouseEvent) {}
+
+const data = {
+  aaa: "12345",
+  bbb: 12345,
+};
+
+function download(event: MouseEvent) {
+  console.log("aaa");
+  const a = document.createElement("a");
+  a.href = URL.createObjectURL(new Blob([JSON.stringify(data, null, 2)], { type: "application/json" }));
+  a.download = `${data.aaa}.json`;
+  a.dispatchEvent(new MouseEvent("click"));
+}
+</script>
 
 <template>
   <main>
     <h2>Home</h2>
+    <button @click="upload">Upload JSON</button>
+    <button @click="download">Download JSON</button>
   </main>
 </template>

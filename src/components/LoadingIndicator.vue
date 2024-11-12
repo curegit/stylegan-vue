c559f7bd96276aab257aba944da53e0d186a632b
curegit <37978051+curegit@users.noreply.github.com>
2024-11-12 17:30:13 +0900

    Refactor loading indicator styles and props

--- src/components/LoadingIndicator.vue
+++ src/components/LoadingIndicator.vue
@@ -1,40 +1,56 @@
 <script setup lang="ts">
-defineProps<{
+const props = defineProps<{
   size?: string;
   thickness?: string;
   duration?: number;
 }>();
+
+const durationText = props.duration !== undefined && props.duration !== null ? `${props.duration}s` : props.duration;
 </script>
 
 <template>
-  <div class="indicator">
-    <div class="circle"></div>
-    <div class="message">
-      <slot>Loading...</slot>
+  <div class="loading-indicator">
+    <div class="loading-circle"></div>
+    <div class="loading-message">
+      <slot>Loading ...</slot>
     </div>
   </div>
 </template>
 
+<style>
+:root {
+  --loading-indicator-size-default: 32px;
+  --loading-indicator-thickness-default: 4px;
+  --loading-indicator-duration-default: 0.8s;
+}
+</style>
+
 <style scoped>
-.indicator {
+.loading-indicator {
+  --size: v-bind("size ?? 'var(--loading-indicator-size-default)'");
+  --thickness: v-bind("thickness ?? 'var(--loading-indicator-thickness-default)'");
+  --duration: v-bind("durationText ?? 'var(--loading-indicator-duration-default)'");
+}
+
+.loading-indicator {
   display: flex;
   flex-flow: column wrap;
   align-items: center;
 }
 
-.message {
+.loading-message {
   color: #333;
   font-size: 90%;
 }
 
-.circle {
-  width: v-bind("size ?? '32px'");
-  height: v-bind("size ?? '32px'");
+.loading-circle {
+  width: var(--size);
+  height: var(--size);
   margin: 3px;
-  border: v-bind("thickness ?? '4px'") rgba(0, 0, 0, 0.25) solid;
-  border-top: v-bind("thickness ?? '4px'") black solid;
+  border: var(--thickness) rgba(0, 0, 0, 0.25) solid;
+  border-top: var(--thickness) black solid;
   border-radius: 50%;
-  animation: rotation v-bind("`${duration ?? 0.8}s`") infinite linear;
+  animation: rotation var(--duration) infinite linear;
 }
 
 @keyframes rotation {

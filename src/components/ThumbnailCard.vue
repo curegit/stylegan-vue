56f14492c8b411936909f81e88605341177af8e6
curegit <37978051+curegit@users.noreply.github.com>
2024-11-14 15:43:31 +0900

    Refactor ModelCard to ThumbnailCard component

---
+++ src/components/ThumbnailCard.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+const props = defineProps<{
+  thumbnail: string;
+  thumbnailRatio?: number;
+  square?: boolean;
+  fit?: "fill" | "contain" | "cover" | "none" | "scale-down";
+}>();
+
+const { fit = "fill", thumbnailRatio = 0.5 } = props;
+</script>
+
+<template>
+  <div class="thumbnail-card">
+    <img class="thumbnail-card-image" :src="thumbnail" :class="{ square }" />
+    <div class="thumbnail-card-content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.thumbnail-card {
+  display: grid;
+  grid-template-columns: v-bind("`${thumbnailRatio}fr 1fr`");
+}
+
+.thumbnail-card-image {
+  width: 100%;
+  height: auto;
+  object-fit: v-bind(fit);
+}
+
+.thumbnail-card-image.square {
+  aspect-ratio: 1 / 1;
+}
+</style>
+
+<style>
+:where(.thumbnail-card) {
+  gap: 10px;
+  padding: 10px;
+  border-radius: 8px;
+  background: linear-gradient(0deg, #ffffff70, #ffffffbf);
+}
+
+:where(.thumbnail-card-image) {
+  border-radius: 8px;
+}
+</style>
